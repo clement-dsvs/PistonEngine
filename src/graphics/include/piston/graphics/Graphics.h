d32ae7d9b90a977b14db29ba0142727dd1adc962
Clement DESAVIS <clement.desavis@outlook.com>
2024-12-19 00:05:06 +0100

    restructure project in modules

---
+++ src/graphics/include/piston/graphics/Graphics.h
@@ -0,0 +1,16 @@
+//
+// Created by cleme on 18/12/2024.
+//
+
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+
+
+class Graphics {
+
+};
+
+
+
+#endif //GRAPHICS_H
---
+++ src/network/include/piston/network/Network.h
@@ -0,0 +1,16 @@
+//
+// Created by cleme on 18/12/2024.
+//
+
+#ifndef NETWORK_H
+#define NETWORK_H
+
+
+
+class Network {
+
+};
+
+
+
+#endif //NETWORK_H
