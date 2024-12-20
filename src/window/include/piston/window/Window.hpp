4663d531cd1dbd310966565839169210be352100
Clement DESAVIS <clement.desavis@outlook.com>
2024-12-20 11:56:03 +0100

    created module "window"

---
+++ src/window/include/piston/window/Window.hpp
@@ -0,0 +1,14 @@
+//
+// Created by cleme on 20/12/2024.
+//
+
+#ifndef WINDOW_HPP
+#define WINDOW_HPP
+
+namespace piston {
+    class Window {
+        Window();
+    };
+}
+
+#endif //WINDOW_HPP
---
+++ src/window/src/Window.cpp
@@ -0,0 +1,8 @@
+//
+// Created by cleme on 20/12/2024.
+//
+
+#include "piston/window/Window.hpp"
+
+piston::Window::Window() {
+}
